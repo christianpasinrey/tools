3633de44fa15fd6909b0f8bfd0a8d582a2db2540
Christian Pasín Rey <christian.pasin.rey@gmail.com>
2026-01-21 09:17:56 +0100

    feat: agregar Lightroom a la categoría de productividad y Premiere y After Effects a la categoría de creativos

--- src/data/cheatsheets/index.js
+++ src/data/cheatsheets/index.js
@@ -131,13 +131,13 @@ export const categories = [
     id: 'productivity',
     name: 'Productividad',
     icon: 'M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z',
-    sheets: ['excel', 'notion', 'photoshop']
+    sheets: ['excel', 'notion', 'photoshop', 'lightroom']
   },
   {
     id: 'creative',
     name: 'Creativos',
     icon: 'M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z',
-    sheets: ['music', 'photography']
+    sheets: ['music', 'photography', 'premiere', 'aftereffects']
   }
 ]
 
