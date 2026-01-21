3607af01252f3f4d73f29365842dc6abdbf31431
Christian <christian.pasin.rey@gmail.com>
2026-01-21 20:41:17 +0100

    feat: agregar comportamiento de desplazamiento al enrutador

--- src/router/index.js
+++ src/router/index.js
@@ -66,7 +66,13 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return { top: 0 }
+  }
 })
 
 export default router
